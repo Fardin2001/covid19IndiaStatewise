2ada0e06ee9e8d35c71df83ec6d22ff37f420d7f
Fardin khan <48712100+Fardin2001@users.noreply.github.com>
2020-04-19 01:03:01 +0530

    Add files via upload

---
+++ style.php
@@ -0,0 +1,30 @@
+<style>
+*{margin: 0;padding: 0;box-sizing: border-box;font-family: 'Muli', sans-serif;}
+.line{
+text-decoration: underline;
+
+}
+
+.search-box{
+border:solid #343A40 2px;
+
+}
+#India{
+width: 25px;
+
+}
+
+.corona-update h5{
+	text-decoration: underline;
+}
+
+@media only screen and (min-width: 480px){
+.scrollLeft{
+
+		display:none;
+
+	}
+}
+
+
+</style>
